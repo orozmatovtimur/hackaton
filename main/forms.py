4d5f8e91f34d65732c399c7e59837c4eaf041564
Timur <tima@MacBook-Air-Timur.local>
2021-08-03 16:54:05 +0600

    Add registrtion form end templates

--- main/forms.py
+++ main/forms.py
@@ -13,3 +13,17 @@ class UpdateDishForm(forms.ModelForm):
     class Meta:
         model = Dish
         fields = "__all__"
+
+
+# class ReviewForm(forms.ModelForm):
+#     """Форма отзыва"""
+#     captcha = ReCaptchaField()
+#
+#     class Meta:
+#         model = Reviews
+#         fields = ('name', 'email', 'text', 'captcha')
+#         widgets = {
+#             'name': forms.TextInput(attrs={'class': 'form-control border'}),
+#             'email': forms.EmailField(attrs={'class': 'form-control border'}),
+#             'text': forms.Textarea(attrs={'class': 'form-control border'}),
+#         }
--- main/views.py
+++ main/views.py
@@ -7,9 +7,10 @@ from main.models import *
 
 
 class MainPageView(ListView):
-    model = Dish
+    model = Category
     template_name = 'base.html'
-    context_object_name = 'dish'
+    context_object_name = 'categories'
+
 
 
 class DishCreateView(CreateView):
@@ -46,3 +47,15 @@ class DishDeleteView(DeleteView):
 
     def get_success_url(self):
         return reverse('home')
+
+
+class AddReview(View):
+
+    def dish(self, request, pk):
+        form = ReviewForm(request.POST)
+        dish = Dish.objects.get(id=pk)
+        if form.is_valid():
+            form = form.save(commit=False)
+            form.dish = dish
+            form.save()
+        return redirect(dish.get_absolute_url())
