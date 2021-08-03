91e7ae5d5379ffc365978d0bdc598f2170f61a64
bakayn29 <bakyn5@gmail.com>
2021-08-03 14:01:38 +0600

    ads

--- cafe_shop/settings.py
+++ cafe_shop/settings.py
@@ -144,3 +144,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+
--- main/context_proccesor.py
+++ main/context_proccesor.py
@@ -2,7 +2,7 @@ from .models import Category
 
 
 def get_categories(request):
-    print('12345678')
+    # print('12345678')
     categories = Category.objects.all()
-    print('12345678')
+    # print('12345678')
     return {'categories': categories}
