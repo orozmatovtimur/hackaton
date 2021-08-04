38aa4867e20901ee2911fb107ea53eff97a8f7ae
bakayn29 <bakyn5@gmail.com>
2021-08-04 16:50:44 +0600

    sdd

--- main/urls.py
+++ main/urls.py
@@ -4,7 +4,7 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('', MainPageView.as_view(), name='home'),
+    path('home/', MainPageView.as_view(), name='home'),
     path('dish/create/', DishCreateView.as_view(), name='create_dish'),
     path('dish/update/<int:id>/', DishUpdateView.as_view(), name='update_dish'),
     path('dish/delete/<int:id>/', DishDeleteView.as_view(), name='delete_dish'),
