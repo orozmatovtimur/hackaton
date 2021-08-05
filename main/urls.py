9b3131cd376258630a0d3567aa039247066653c5
bakayn29 <bakyn5@gmail.com>
2021-08-05 11:09:38 +0600

    123456

--- main/urls.py
+++ main/urls.py
@@ -3,15 +3,21 @@ from django.urls import path
 from main.views import *
 
 urlpatterns = [
-    path('', MainPageView.as_view(), name='home'),
+    path('home/', MainPageView.as_view(), name='home'),
+    path('<str:slug>/', DishListView.as_view(), name='list'),
+    path('dish/<int:id>/', DishDetailView.as_view(), name='detail'),
     path('dish/create/', DishCreateView.as_view(), name='create_dish'),
     path('dish/update/<int:id>/', DishUpdateView.as_view(), name='update_dish'),
     path('dish/delete/<int:id>/', DishDeleteView.as_view(), name='delete_dish'),
 
-# from django.contrib import admin
-# from django.urls import path, include
-#
-# urlpatterns = [
-#     path('/', )
+    # cart urls
+    path('cart/add/<int:id>/', cart_add, name='cart_add'),
+    path('cart/item_clear/<int:id>/', item_clear, name='item_clear'),
+    path('cart/item_increment/<int:id>/',
+         item_increment, name='item_increment'),
+    path('cart/item_decrement/<int:id>/',
+         item_decrement, name='item_decrement'),
+    path('cart/cart_clear/', cart_clear, name='cart_clear'),
+    path('cart/cart-detail/', cart_detail, name='cart_detail'),
 ]
 
--- main/views.py
+++ main/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.urls import reverse
 from django.views.generic import *
+from django.contrib.auth.decorators import login_required
+
 
 from main.forms import *
 from main.models import *
@@ -11,6 +13,28 @@ class MainPageView(ListView):
     template_name = 'base.html'
     context_object_name = 'categories'
 
+
+class DishListView(ListView):
+    model = Dish
+    template_name = 'list_dish.html'
+    context_object_name = 'dishes'
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        slug = self.kwargs.get('slug')
+        queryset = queryset.filter(category__slug=slug)
+        return queryset
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['category'] = self.kwargs.get('slug')
+        return context
+
+class DishDetailView(DetailView):
+    model = Dish
+    template_name = 'detail_dish.html'
+    context_object_name = 'dishess'
+    pk_url_kwarg = 'id'
 
 
 class DishCreateView(CreateView):
@@ -39,6 +63,9 @@ class DishUpdateView(UpdateView):
         context['dish_form'] = self.get_form(self.get_form_class())
         return context
 
+    def get_success_url(self):
+        return reverse('home')
+
 
 class DishDeleteView(DeleteView):
     model = Dish
@@ -49,13 +76,57 @@ class DishDeleteView(DeleteView):
         return reverse('home')
 
 
-class AddReview(View):
+# class AddReview(View):
+#
+#     def dish(self, request, pk):
+#         form = ReviewForm(request.POST)
+#         dish = Dish.objects.get(id=pk)
+#         if form.is_valid():
+#             form = form.save(commit=False)
+#             form.dish = dish
+#             form.save()
+#         return redirect(dish.get_absolute_url())
 
-    def dish(self, request, pk):
-        form = ReviewForm(request.POST)
-        dish = Dish.objects.get(id=pk)
-        if form.is_valid():
-            form = form.save(commit=False)
-            form.dish = dish
-            form.save()
-        return redirect(dish.get_absolute_url())
+
+@login_required()
+def cart_add(request, id):
+    cart = Cart(request)
+    product = Product.objects.get(id=id)
+    cart.add(product=product)
+    return redirect("home")
+
+
+@login_required()
+def item_clear(request, id):
+    cart = Cart(request)
+    product = Product.objects.get(id=id)
+    cart.remove(product)
+    return redirect("cart_detail")
+
+
+@login_required()
+def item_increment(request, id):
+    cart = Cart(request)
+    product = Product.objects.get(id=id)
+    cart.add(product=product)
+    return redirect("cart_detail")
+
+
+@login_required()
+def item_decrement(request, id):
+    cart = Cart(request)
+    product = Product.objects.get(id=id)
+    cart.decrement(product=product)
+    return redirect("cart_detail")
+
+
+@login_required()
+def cart_clear(request):
+    cart = Cart(request)
+    cart.clear()
+    return redirect("cart_detail")
+
+
+@login_required()
+def cart_detail(request):
+    return render(request, 'cart/cart_detail.html')
